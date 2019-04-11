28aad0f1a9312dcc26d49799b2830504da0371f5
League <league@WTS9.attlocal.net>
2019-04-10 18:19:38 -0700

    n
    n

---
+++ src/extra/nastySurprise.java
@@ -0,0 +1,28 @@
+package extra;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class nastySurprise {
+	public static void main(String[] args) {
+		
+	}
+	private void showPictureFromTheInternet(String imageUrl) {
+	     try {
+	          URL url = new URL(imageUrl);
+	          Icon icon = new ImageIcon(url);
+	          JLabel imageLabel = new JLabel(icon);
+	          JFrame frame = new JFrame();
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (MalformedURLException e) {
+	          e.printStackTrace();
+	     }
+	}
+}
