50180da83f41b4ce6a0f3c1a3b1ab04ec0facab0
League Student <league@iMac-17.attlocal.net>
2019-06-19 16:19:04 -0700

    chuckle clicker fin

--- src/extra/ChuckleClicker.java
+++ src/extra/ChuckleClicker.java
@@ -1,5 +1,6 @@
 package extra;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,7 +14,8 @@ public class ChuckleClicker implements ActionListener {
 	JPanel jp = new JPanel();
 	JButton jb = new JButton();
 	JButton jb1 = new JButton();
-
+	JButton jb2 = new JButton();
+	JButton jb3 = new JButton();
 	public static void main(String[] args) {
 		ChuckleClicker mb = new ChuckleClicker();
 		mb.makeButtons();
@@ -25,12 +27,19 @@ public class ChuckleClicker implements ActionListener {
 		jf.add(jp);
 		jp.add(jb);
 		jp.add(jb1);
+		jp.add(jb2);
+		jp.add(jb3);
+		jp.setLayout(new GridLayout(2,2));
 		jb.setText("joke");
 		jb1.setText("punchline");
+		jb2.setText("joke");
+		jb3.setText("punchline");
 		jf.setVisible(true);
 		jf.pack();
 		jb.addActionListener(this);
 		jb1.addActionListener(this);
+		jb2.addActionListener(this);
+		jb3.addActionListener(this);
 
 	}
 
@@ -39,10 +48,18 @@ public class ChuckleClicker implements ActionListener {
 
 		if (e.getSource() == jb) {
 			JOptionPane.showMessageDialog(null, "What’s the best part about living in Switzerland?");
-		} else {
+		} if (e.getSource() == jb1) {
 			JOptionPane.showMessageDialog(null, "I don't know but the flag is a big plus");
 
 		}
+		
+		if (e.getSource() == jb2) {
+			JOptionPane.showMessageDialog(null, "waht did the traffic light say to the car?");
+		} if (e.getSource() == jb3) {
+			JOptionPane.showMessageDialog(null, "Dont look im about to change");
+
+		}
+		
 
 	}
 }
