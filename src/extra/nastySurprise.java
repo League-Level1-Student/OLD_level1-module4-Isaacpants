c9a1a87f719f3dc93f42ba321183f01e91623d20
League Student <league@iMac.attlocal.net>
2019-06-20 15:33:07 -0700

    nastSurprise.fin

--- src/extra/nastySurprise.java
+++ src/extra/nastySurprise.java
@@ -1,16 +1,52 @@
 package extra;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class nastySurprise {
-	public static void main(String[] args) {
+public class nastySurprise implements ActionListener{
+	JFrame jf = new JFrame();
+	JPanel jp = new JPanel();
+	JButton jb = new JButton();
+	JButton jb1 = new JButton();
+	nastySurprise(){
+	
 		
+		
+		jf.setVisible(true);
+		jf.add(jp);
+		
+		
+		jp.add(jb);
+		jp.add(jb1);
+		
+		
+		jb.setText("trick");
+		jb1.setText("treat");
+		jb.addActionListener(this);
+		jb1.addActionListener(this);
+
+		
+		
+		jf.pack();
+	}
+	
+	public static void main(String[] args) {
+
+		
+		nastySurprise n = new nastySurprise();
+		
+		
+		
+	
 	}
 	private void showPictureFromTheInternet(String imageUrl) {
 	     try {
@@ -24,5 +60,16 @@ public class nastySurprise {
 	     } catch (MalformedURLException e) {
 	          e.printStackTrace();
 	     }
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == jb1) {
+			showPictureFromTheInternet("https://www.thelabradorsite.com/wp-content/uploads/2019/03/Cute-puppy-Names-Over-200-Adorable-Ideas-LS-long.jpg");
+		
+		}
+		if (e.getSource() == jb) {
+			showPictureFromTheInternet("https://image.shutterstock.com/image-illustration/3d-illustration-scary-ghost-woman-260nw-699737932.jpg");
+		}
 	}
 }
