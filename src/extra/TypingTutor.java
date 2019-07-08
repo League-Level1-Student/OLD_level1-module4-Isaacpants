1ec16854061edef69c06e5c28e49d241999687d9
League Student <league@WTS9.attlocal.net>
2019-07-08 15:54:34 -0700

    typing tutor fin

--- src/extra/TypingTutor.java
+++ src/extra/TypingTutor.java
@@ -3,16 +3,20 @@ package extra;
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class TypingTutor implements KeyListener {
 	JFrame jf= new JFrame();
 	JLabel jl = new JLabel();
 	JPanel jp = new JPanel();
+	int numcor = 0;
+	int num = 0;
 	char currentLetter;
 	
 	TypingTutor(){
@@ -70,7 +74,10 @@ public class TypingTutor implements KeyListener {
 		
 		System.out.println("you typed : "+e.getKeyChar());
 		
-	
+		num++;
+	if(num>=10) {
+		showTypingSpeed(numcor);
+	}
 	}
 
 
@@ -84,6 +91,7 @@ public class TypingTutor implements KeyListener {
 if(e.getKeyChar()==currentLetter) {
 			System.out.println("u r correct");
 			jp.setBackground(Color.green);
+			numcor++;
 		}else {
 			
 			jp.setBackground(Color.red);
@@ -95,4 +103,15 @@ if(e.getKeyChar()==currentLetter) {
 
 		
 	}
+	Date timeAtStart = new Date();
+
+	private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+	      Date timeAtEnd = new Date();
+	      long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+	      long gameInSeconds = (gameDuration / 1000) % 60;
+	      double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+	      int charactersPerMinute = (int) (charactersPerSecond * 60);
+	      JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
+	}
 }
+
