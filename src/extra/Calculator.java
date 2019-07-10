8f5bd9fd26ab392ec0d363c36c12981de8681a48
League Student <league@iMac-9.attlocal.net>
2019-07-10 15:33:35 -0700

    calculator fin

--- src/extra/Calculator.java
+++ src/extra/Calculator.java
@@ -1,33 +1,42 @@
 package extra;
 
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Calculator {
+public class Calculator implements ActionListener{
 	
 	
-	static JFrame jf ;
-	static JPanel jp ;
-	static JLabel jl ;
-	static JButton jb;
-	static JButton jb1;
-	static JButton jb2;
-	static JButton jb3;
-	static JTextField jtf;
-	static JTextField jtf1;
-public static void main(String[] args) {
-	 jf = new JFrame();
-	 jp = new JPanel();
-	 jl = new JLabel();
-	 jtf = new JTextField();
-	 jtf1 = new JTextField();
-	 jb = new JButton();
-	 jb1 = new JButton();
-	 jb2 = new JButton();
-	 jb3 = new JButton();
+	 JFrame jf = new JFrame();
+	 JPanel jp = new JPanel();
+	 JLabel jl = new JLabel();
+	 JButton jb= new JButton();
+	 JButton jb1= new JButton();
+	 JButton jb2= new JButton();
+	 JButton jb3= new JButton();
+	 JTextField jtf= new JTextField();
+	 JTextField jtf1= new JTextField();
+	
+	
+	 public void setup() {
+	
+ jb.addActionListener(this);
+	 
+	 
+	 jb1.addActionListener(this);
+	 
+
+	 jb2.addActionListener(this);
+	 
+
+	 jb3.addActionListener(this);
 	 
 	 jf.setVisible(true);
 	 jf.setSize(450, 500);
@@ -37,6 +46,8 @@ public static void main(String[] args) {
 	 jtf1.setColumns(15);
 	 jtf.setSize(100, 50);
 	 jtf1.setSize(100, 50);
+jl.setPreferredSize(new Dimension(50,50));
+	 jl.setFont(new Font("Serif", Font.BOLD, 50));
 	 
 	 jf.add(jp);
 	 jp.add(jtf);
@@ -45,17 +56,36 @@ public static void main(String[] args) {
 	 jp.add(jb1);
 	 jp.add(jb2);
 	 jp.add(jb3);
+	 jp.add(jl);
+	 
+	
+	 
 	 
 
 	 
 	 jb.setText("add");
+	 jb.addActionListener(this);
+	 
 	 jb1.setText("sub");
+	 jb1.addActionListener(this);
+	 
 	 jb2.setText("mul");
+	 jb2.addActionListener(this);
+	 
 	 jb3.setText("div");
+	 jb3.addActionListener(this);
 	 
 	 
+	 jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 }
+public static void main(String[] args) {
+	Calculator calc = new Calculator();
+	calc.setup();
+	
 	 
+
 	 
+
 	 
 }public void add() {
 	int num = Integer.parseInt(jtf.getText());
@@ -79,6 +109,21 @@ public static void main(String[] args) {
 	int num1 = Integer.parseInt(jtf1.getText());
 	int num2 = num/num1;
 	jl.setText(""+num2);
+	
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+	if(e.getSource()==jb) {
+		add();
+	}if(e.getSource()==jb1) {
+		sub();
+	}if(e.getSource()==jb2) {
+		mul();
+	}if(e.getSource()==jb3) {
+		div();
+	}
 	
 }
 
