43bad902b73043b374d2c8f4413f282e64ddb254
League Student <league@iMac-24.attlocal.net>
2019-08-05 16:11:39 -0700

    book of illusions fin

--- src/extra2/BookOfIllusions.java
+++ src/extra2/BookOfIllusions.java
@@ -18,7 +18,7 @@ public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
 	JFrame jf = new JFrame();
-
+JLabel jl;
 	public void run() {
 		// 2. make the frame visible
 		jf.setVisible(true);
@@ -26,16 +26,16 @@ public class BookOfIllusions extends MouseAdapter {
 		jf.setSize(1000, 1000);
 		// 4. find 2 images and save them to your project’s default package
 		// done
-
+jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
 		String pic = "cherry.jpg";
-		String pic1 = "watermelon.jpg";
+		
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
-		JLabel jl;
-		JLabel jl1;
+	
+		
 		// 7. use the "loadImage..." methods below to initialize your JLabel
 		jl = loadImageFromComputer("cherry.jpg");
-		jl1 = loadImageFromComputer("watermelon.jpg");
+	
 		// 8. add your JLabel to the frame
 		jf.add(jl);
 		// 9. call the pack() method on the frame
@@ -48,8 +48,13 @@ public class BookOfIllusions extends MouseAdapter {
 		// 11. Print "clicked!" to the console when the mouse is pressed
 		System.out.println("clicked!");
 		// 12. remove everything from the frame that was added earlier
-		jf.remove(jf);
+		jf.remove(jl);
 		// 13. load a new image like before (this is more than one line of code)
+		String pic1 = "watermelon.jpg";
+		JLabel jl1;
+		jl1 = loadImageFromComputer("watermelon.jpeg");
+		jf.add(jl1);
+		
 		// 14. pack the frame
 		jf.pack();
 	}
---
+++ src/extra2/BookOfIllusionsRunner.java
@@ -0,0 +1,7 @@
+package extra2;
+
+public class BookOfIllusionsRunner {
+	public static void main(String[] args)  {
+		new BookOfIllusions().run();
+}
+}
