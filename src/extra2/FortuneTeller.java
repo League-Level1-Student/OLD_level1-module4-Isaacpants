6fb617a82bd0177cbab5a67ebcf90f00b265c453
League Student <league@iMac-5.attlocal.net>
2019-08-06 15:25:16 -0700

    fortune teller fin magic 8 ball fin

--- src/extra2/FortuneTeller.java
+++ src/extra2/FortuneTeller.java
@@ -50,12 +50,41 @@ System.out.println("x: "+mouseX+" y: "+mouseY);
 //done
 
    	 // 7. Adjust your secret location co-ordinates here:
-   	 int secretLocationX = 0;
-   	 int secretLocationY = 0;
+   	 int secretLocationX = 250;
+   	 int secretLocationY = 250;
    	 /** If the mouse co-ordinates and secret location are close, we'll let them ask a question. */
    	 if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
    		 // 8. Get the user to enter a question for the fortune teller
-JOptionPane.showInputDialog("ask me something");
+   		int r = new Random().nextInt(4);
+   	// 3. Print out this variable
+   	System.out.println(r);
+   	// 4. Get the user to enter a question for the 8 ball
+   	JOptionPane.showInputDialog("ask me a question");
+   	// 5. If the random number is 0
+   	if(r==0) {
+   	// -- tell the user "Yes"
+   	JOptionPane.showMessageDialog(null, "Yes");
+   	}
+   	// 6. If the random number is 1
+   	if(r==1) {
+   	// -- tell the user "No"
+   	JOptionPane.showMessageDialog(null, "NO");
+   	}
+
+
+   	// 7. If the random number is 2
+   	if(r==2) {
+   	// -- tell the user "Maybe you should ask Google?"
+   	JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");
+   	}
+
+
+   	// 8. If the random number is 3
+   	if(r==3) {
+   	// -- write your own answer
+   	JOptionPane.showMessageDialog(null, "unclear ask again later");
+   	}
+
    		 // 9. Find a spooky sound and put it in your default package (freesound.org)
 
 //done
@@ -68,9 +97,16 @@ sound.play();
 pause(3);
    		 // 12. Insert your completed Magic 8 ball code here
 
-   	 }
 
-    }
+
+}
+
+}
+
+
+   	 
+
+    
 
     private boolean areClose(int mouseX, int secretLocationX) {
    	 return mouseX < secretLocationX + 15 && mouseX > secretLocationX - 15;
